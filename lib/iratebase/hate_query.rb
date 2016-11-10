7694ca9b3203566fad9feef4ce6859e8ef72f84a
Ben! <benpolson.zena@gmail.com>
2016-11-10 17:41:50 -0600

    basic setters getters and tests

--- lib/iratebase/hate_query.rb
+++ lib/iratebase/hate_query.rb
@@ -1,11 +1,39 @@
 class HateQuery
   def initialize(key = "")
-    @key = "#{key}"
+    self.key = key
+    @version = 3
+    @subversion = 0
+    @query_type = nil
+    @output = :json
   end
+
+  # Class methods
   def self.valid_key(key)
     /[0-9a-f]{32}/ === key
   end
+
+  # Setters and getters
   def key
     return String.new(@key)
+  end
+
+  def key=(key)
+    if key == "" || HateQuery.valid_key(key)
+      @key = "#{key}"
+    else
+      @key = ""
+    end
+  end
+
+  def version
+    "v" + @version.to_s + "-" + @subversion.to_s
+  end
+
+  def query_type
+    @query_type.to_s
+  end
+
+  def output
+    @output.to_s
   end
 end
--- spec/hate_query_spec.rb
+++ spec/hate_query_spec.rb
@@ -1,10 +1,21 @@
 require "iratebase/hate_query"
 
 describe HateQuery do
-  context "when no key given" do
+  context "when creating a new query and no key is given" do
+    def new_query
+      HateQuery.new
+    end
     it "should initialize an empty string" do
-      hq = HateQuery.new
-      expect(hq.key).to eql ""
+      expect(new_query.key).to eql ""
+    end
+    it "should set the version to the default" do
+      expect(new_query.version).to eql "v3-0"
+    end
+    it "should not assume query type" do
+      expect(new_query.query_type).to eql ""
+    end
+    it "should set default output as json" do
+      expect(new_query.output).to eql "json"
     end
   end
   it "should know what a valid key looks like" do
@@ -13,6 +24,8 @@ describe HateQuery do
     key = "1"
     expect(HateQuery.valid_key(key)).to be false
     key = "fedcba0987654321fedcba098765432"
+    expect(HateQuery.valid_key(key)).to be false
+    key = "1234567890abcdef1234567890abcdeg"
     expect(HateQuery.valid_key(key)).to be false
   end
 end
